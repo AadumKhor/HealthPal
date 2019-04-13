3c6d698998d492795ad88463982c71e803c1697f
ritik-rathi <rathi27ritik@gmail.com>
2019-04-13 16:06:23 +0530

    animated container

--- lib/Screens/homescreen.dart
+++ lib/Screens/homescreen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:healthpal/main.dart';
 
 
 class HomeScreen extends StatefulWidget {
@@ -35,3 +35,72 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 }
 
+int show = 0;
+var height = 50.0;
+Icon icon = Icon(Icons.arrow_downward);
+
+Widget content() {
+  if (show == 1) {
+    return TextField(
+      decoration: InputDecoration(hintText: 'Enter food item'),
+    );
+  }
+  return SizedBox(height: 1);
+}
+
+class Cards extends StatefulWidget {
+  @override
+  _CardsState createState() => _CardsState();
+}
+
+class _CardsState extends State<Cards> {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      shape: BeveledRectangleBorder(
+        borderRadius: BorderRadius.circular(15),
+      ),
+      elevation: 10,
+      margin: EdgeInsets.only(left: 16, right: 16, top: 8),
+      child: GestureDetector(
+        onTap: () {
+          color = Colors.indigo;
+          height = 70;
+          icon = Icon(Icons.arrow_upward);
+          show = 1;
+          setState(() {});
+        },
+        child: AnimatedContainer(
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15), color: color),
+            duration: Duration(seconds: 1),
+            height: height,
+            child: Container(
+              child: Column(
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Text('Breakfast',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 20)),
+                      SizedBox(width: 50),
+                      IconButton(
+                        icon: Icon(Icons.add),
+                        onPressed: () {
+                          color = Colors.indigo[300];
+                          height = 70;
+                          icon = Icon(Icons.arrow_downward);
+                          show = 0;
+                          setState(() {});
+                        },
+                      ),
+                    ],
+                  ),
+                  content()
+                ],
+              ),
+            )),
+      ),
+    );
+  }
+}
