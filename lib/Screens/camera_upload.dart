4fb5ba7479cecd96c3b300b10227bee3751f9b51
Aayush Malhotra <37381075+AadumKhor@users.noreply.github.com>
2019-04-13 16:48:25 +0530

    Camera upload start

---
+++ lib/Screens/camera_upload.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class CameraUpload extends StatefulWidget {
+  @override
+  _CameraUploadState createState() => _CameraUploadState();
+}
+
+class _CameraUploadState extends State<CameraUpload> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      
+    );
+  }
+}
