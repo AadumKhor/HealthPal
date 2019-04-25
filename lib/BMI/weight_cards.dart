204dc3c08fa324ee1f1ce2482f40de602de1c366
Aayush Malhotra <37381075+AadumKhor@users.noreply.github.com>
2019-04-25 23:53:17 +0530

    Ainvayi vala commit

--- lib/BMI/weight_cards.dart
+++ lib/BMI/weight_cards.dart
@@ -9,33 +9,47 @@ class WeightCard extends StatefulWidget {
 }
 
 class _WeightCardState extends State<WeightCard> {
+
+  int weight;
+
   @override
   Widget build(BuildContext context) {
     return Card(
       child: Padding(
         padding: EdgeInsets.all(screenAwareSize(32, context)),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-          CardTitle(
-            title: 'WEIGHT',
-            subtitle: 'in KG',
-          ),
-          Expanded(
-            child: Center(
-              child: Padding(
-                padding: EdgeInsets.symmetric(horizontal: 16.0),
-                child: _drawSlider(),
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              CardTitle(
+                title: 'WEIGHT',
+                subtitle: 'in KG',
               ),
-            ),
-          )
-        ]),
+              Expanded(
+                child: Center(
+                  child: Padding(
+                    padding:
+                        EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
+                    child: _drawSlider(),
+                  ),
+                ),
+              )
+            ]),
       ),
     );
   }
 
   Widget _drawSlider() {
-    return WeightBackground();
+    return WeightBackground(
+      child: LayoutBuilder(
+        builder: (context , constraints)=> WeightSlider(
+          minValue: 30,
+          maxValue: 110,
+          value: weight,
+          onChanged: (val) => setState(() => weight = val),
+          width: constraints.maxWidth
+        ),
+      ),
+    );
   }
 }
 
@@ -52,6 +66,7 @@ class WeightBackground extends StatelessWidget {
       children: <Widget>[
         Container(
           height: screenAwareSize(100.0, context),
+          width: double.infinity,
           decoration: BoxDecoration(
             color: Color.fromRGBO(244, 244, 244, 1.0),
             borderRadius: BorderRadius.circular(screenAwareSize(50, context)),
@@ -65,5 +80,83 @@ class WeightBackground extends StatelessWidget {
         ),
       ],
     );
+  }
+}
+// just a comment
+class WeightSlider extends StatelessWidget {
+  int maxValue, minValue , value;
+  final ValueChanged<int> onChanged;
+  double width;
+
+  WeightSlider({Key key, this.maxValue, this.minValue, this.width ,  this.onChanged , this.value})
+      : super(key: key);
+
+  double get itemExtent => width / 3;
+  int _indexToValue(int index) => minValue + (index - 1);
+
+  @override
+  Widget build(BuildContext context) {
+    int itemCount = (maxValue - minValue) + 3;
+    return NotificationListener(
+      onNotification: _onNotification,
+          child: ListView.builder(
+        itemCount: itemCount,
+        itemExtent: itemExtent,
+        physics: BouncingScrollPhysics(),
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) {
+          final int value = _indexToValue(index);
+          bool isExtra = index == 0 || index == itemCount - 1;
+
+          return isExtra
+              ? new Container()
+              : Center(
+                  child: Text(
+                    value.toString(),
+                    style: _getTextStyle(value),
+                  ),
+                );
+        },
+      ),
+    );
+  }
+
+  int _offsetToMiddleIndex(double offset) => ((width + offset) /2 ) ~/ itemExtent;
+
+  int _offsetToMiddleValue(double offset){
+    int _indexOfMiddleElement = _offsetToMiddleIndex(offset);
+    int middleValue = _indexToValue(_indexOfMiddleElement);
+    return middleValue;
+  }
+
+  bool _onNotification(Notification notification){
+    if(notification is ScrollNotification){
+      int middleValue  = _offsetToMiddleValue(notification.metrics.pixels);
+
+      if(middleValue != value){
+        onChanged(middleValue);
+      }
+    }
+    return true;
+  }
+
+  TextStyle _getDefaultTextStyle() {
+    return new TextStyle(
+      color: Color.fromRGBO(196, 197, 203, 1.0),
+      fontSize: 13.0,
+    );
+  }
+
+  TextStyle _getHighlightedTextStyle(){
+    return new TextStyle(
+      color: Colors.blue,
+      fontSize: 28.0
+    );
+  }
+
+  TextStyle _getTextStyle(int itemValue){
+    return itemValue == value 
+      ? _getHighlightedTextStyle()
+      :_getDefaultTextStyle();
   }
 }
